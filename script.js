dafe65926846886b26b8a5a8dfe9da7f929c4a97
Hayden Nault <naulth@gmail.com>
2023-01-09 12:55:44 -0500

    updated styling, added menu

--- script.js
+++ script.js
@@ -1,4 +1,14 @@
-const h2 = document.createElement("h2");
+/*const h2 = document.createElement("h2");
 h2.textContent = "This content added by JavaScript";
 
 document.querySelector("body").appendChild(h2);
+*/
+
+const input = document.getElementsByClassName('about');
+
+function clickAlert() {
+  alert('I was clicked!');
+}
+
+input.addEventListener('click', clickAlert);
+
